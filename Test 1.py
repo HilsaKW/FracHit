2eac16370fdd31ce8385ea5365e5e557cd744936
HilsaKW <103575975+HilsaKW@users.noreply.github.com>
2022-04-19 09:20:26 -0300

    Update Test 1.py

--- Test 1.py
+++ Test 1.py
@@ -1,9 +1,1 @@
-#este es un archivo de prueba
-
-#primer cambio hecho en Prueba 1 del Main Test 1
- 
- #cambio desde vscode
- 
- #vuelvo a intentar
- 
- #este es un cambio desde GitHub
+#Cambio 1 desde github: commit
